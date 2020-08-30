93978b205137b000a1153c91c18b98730c2f8231
Ankit <ankitshaw42@protonmail.com>
2020-08-30 21:11:18 +0530

    complete app header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+
+import './Header.css';
+
+const Navbar: React.FC = () => {
+  return (
+    <header className="header">
+      <nav className="main-nav">
+        <ul className="main-nav__list">
+          <li>
+            <Link to="/top">
+              <img src="/favicon.png" alt="hacker news icon" />
+            </Link>
+          </li>
+
+          <li>
+            <NavLink to="/top">Top</NavLink>
+          </li>
+          <li>
+            <NavLink to="/new">New</NavLink>
+          </li>
+          <li>
+            <NavLink to="/show">Show</NavLink>
+          </li>
+          <li>
+            <NavLink to="/ask">Ask</NavLink>
+          </li>
+          <li>
+            <NavLink to="/jobs">Jobs</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
